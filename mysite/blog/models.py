19bb77c561a428e0627e305da5500849fbf9bf93
Oksana <oksana2000-02@mail.ru>
2023-10-13 20:33:35 +0300

    create project, create app, create model Post, make migrations, make migrate, setting admin pannel

---
+++ mysite/blog/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Post(models.Model):
+    """
+    Таблица с данными поста
+    """
+
+    class Status(models.TextChoices):
+        """Используемые статусы поста"""
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+
+    class Meta:
+        ordering = ['-publish']  # Сортировка записей в обратном порядке
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+
+    def __str__(self):
+        return self.title
