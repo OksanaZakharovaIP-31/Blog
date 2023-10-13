e3769740d20bb589c9385451f92f7a4efeeedef3
Oksana <oksana2000-02@mail.ru>
2023-10-13 23:21:10 +0300

    add paginator, make migration, change url page

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,19 +1,30 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.published.all()
+    posts_list = Post.published.all()
+    paginator = Paginator(posts_list, 5)
+    page_num = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_num)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html',
                   {'posts': posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                             id=id,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day,
                              status=Post.Status.PUBLISHED)
     return render(request, 'blog/post/detail.html',
                   {'post': post})
