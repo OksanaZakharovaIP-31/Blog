2930e2b077164607cc7988677b6a778a42cecbcd
Oksana <oksana2000-02@mail.ru>
2023-10-13 20:42:41 +0300

    add manager in model.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,6 +4,11 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class PublishManager(models.Manager):
+    """Создание своего менеджера"""
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 
 class Post(models.Model):
     """
@@ -27,6 +32,8 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    objects = models.Manager()  # Использование менежера по умолчанию
+    publish = PublishManager()  # Использование своего менеджера
 
     class Meta:
         ordering = ['-publish']  # Сортировка записей в обратном порядке
